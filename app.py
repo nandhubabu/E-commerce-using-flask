c94ba94dfc5158a7b42e9a485c024804e756f344
Nandhu Babu <158513949+nandhubabu@users.noreply.github.com>
2025-02-02 16:33:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -266,4 +266,4 @@ def payment_cancel():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Get port from environment, default to 5000
-    app.run(host='0.0.0.0', port=port, debug=True
+    app.run(host='0.0.0.0', port=port, debug=True)
